277c778b4a3b832ec289950461f98122319545d1
gganb <gksql0801@naver.com>
2026-05-28 19:51:38 +0900

    feat : 채팅 ui 수정

--- src/component/chat/ChatWindow.jsx
+++ src/component/chat/ChatWindow.jsx
@@ -238,7 +238,9 @@ export default function ChatWindow({ messages, onSendMessage, onRegenerate, isLo
                 className="chat-textarea"
               />
               <button onClick={handleSend} disabled={isLoading} className="chat-send-btn">
-                ↑
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M8 13V3M8 3L4 7M8 3L12 7" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                </svg>
               </button>
             </div>
           </div>
